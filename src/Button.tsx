1fdd6fea4c687484083af93ecc4eaa3625119626
Enn Michael <enntheprogrammer@gmail.com>
2020-03-17 18:56:28 +0100

    Email button is almost fully finished

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,15 +3,15 @@ import './css/Button.scss'
 
 export interface ButtonProps {
     children: React.ReactNode
-    disabled: boolean
-    className: string
+    className?: string
+    disabled?: boolean
 
     onClick(): void
 }
 
 export default function Button(props: ButtonProps) {
     return (
-        <button disabled={props.disabled} onClick={props.onClick} className={props.className}>
+        <button disabled={props.disabled} onClick={props.onClick} className={props.className || ''}>
             {props.children}
         </button>
     )
--- src/EmailButton.tsx
+++ src/EmailButton.tsx
@@ -1,54 +1,66 @@
-import React, {useState} from 'react'
+import React, {RefObject, useRef, useState} from 'react'
 import Button from './Button'
 import './css/EmailButton.scss'
 import {CSSTransition, SwitchTransition} from 'react-transition-group'
 
+const buttonAnimationTimeout = 200
+
 export default function EmailButton() {
     const [showEmail, setShowEmail] = useState(false)
-    const [animateButton, setAnimateButton] = useState(false)
+    const emailInputRef = useRef<HTMLInputElement>(null)
 
-    const [buttonStates, setButtonStates] = useState([
-        {
-            text: 'EMAIL',
-            callback() {
-                setShowEmail(true)
-                setAnimateButton(true)
-            }
-        },
-        {
-            text: 'COPY',
-            callback() {
+    const email = 'bz.ennm@gmail.com'
 
-            }
-        },
-        {
-            text: 'COPIED',
-        }
-    ])
+    const showEmailButton = (
+        <Button onClick={() => setShowEmail(true)}>
+            EMAIL
+        </Button>
+    )
 
-    const currentButtonState = buttonStates[0]
-
-    function onButtonClick() {
-        if (currentButtonState.callback !== undefined) {
-            currentButtonState.callback()
-            setButtonStates(states => states.slice(1))
-        }
-    }
+    const copyEmailButton = <CopyEmailButton email={email} emailInputRef={emailInputRef}/>
 
     return (
         <div className="wrapper">
-            <Button
-                disabled={currentButtonState.callback === undefined}
-                className={animateButton ? 'button-animation' : ''}
-                onClick={onButtonClick}
-            >
-                <SwitchTransition>
-                    <CSSTransition timeout={200} key={currentButtonState.text} classNames="email-button">
-                        <p className="button-text">{currentButtonState.text}</p>
-                    </CSSTransition>
-                </SwitchTransition>
-            </Button>
-            {showEmail && <p className="email">email@email.email</p>}
+            {showEmail ? copyEmailButton : showEmailButton}
+            {showEmail && <input className="email" value={email} readOnly ref={emailInputRef}/>}
         </div>
     )
+}
+
+interface CopyEmailButtonProps {
+    email: string
+    emailInputRef: RefObject<HTMLInputElement>
+}
+
+function CopyEmailButton(props: CopyEmailButtonProps) {
+    const copyText = 'COPY'
+    const doneText = 'DONE'
+
+    const [emailCopied, setCopied] = useState(false)
+    const text = emailCopied ? doneText : copyText
+
+    function onButtonClick() {
+        if (props.emailInputRef.current === null)
+            return
+
+        copyInputToClipboard(props.emailInputRef.current)
+        setCopied(true)
+        setTimeout(() => setCopied(false), 1000)
+    }
+
+    return (
+        <Button disabled={emailCopied} className="button-animation" onClick={onButtonClick}>
+            <SwitchTransition>
+                <CSSTransition appear timeout={buttonAnimationTimeout} key={text} classNames="email-button">
+                    <p className="button-text">{text}</p>
+                </CSSTransition>
+            </SwitchTransition>
+        </Button>
+    )
+}
+
+function copyInputToClipboard(input: HTMLInputElement): void {
+    input.select()
+    document.execCommand('copy')
+    input.setSelectionRange(0, 0)
 }

4ec0d48624675e4a892091f57178148ce9d692c9
Enn Michael <enntheprogrammer@gmail.com>
2020-03-17 22:02:47 +0100

    Leftover stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import EmailButton from "./EmailButton"
+import CVButton from './CVButton'
+import ProjectsButton from "./ProjectsButton";
 import './css/App.scss'
 
 export default function App() {
     return (
         <div className="outer-container">
             <EmailButton/>
+            <CVButton/>
+            <ProjectsButton/>
         </div>
     )
 }

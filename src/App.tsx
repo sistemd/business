a266cd03511e86c34320001dbc2fb87cc5b775d0
Enn Michael <enntheprogrammer@gmail.com>
2020-04-26 21:14:08 +0200

    Fixing up the github page, don't like the animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React, {useState} from 'react'
 import Menu, {ButtonName} from './Menu'
 import './css/App.css'
 import MainContent from './MainContent'
+import Github from './Github'
 
 export default function App() {
     const [selectedButton, setSelectedButton] = useState<ButtonName | undefined>()
+
+    return (
+        <Github />
+    )
 
     return (
         <div>
--- src/Github.tsx
+++ src/Github.tsx
@@ -1,23 +1,11 @@
 import React, {useEffect, useState} from 'react'
-import Button from './Button'
-import Popover from 'react-tiny-popover'
-import 'devicon/devicon-colors.css'
-import 'devicon/devicon.min.css'
-import axios from 'axios'
-import HoverPopover from './HoverPopover'
+import './css/Github.css'
 
 export default function Github() {
-    const [isOpen, setIsOpen] = useState(false)
-    const [projects, setProjects] = useState<readonly GithubProject[]>([])
-
-    useEffect(() => {
-        (async () => setProjects(await getNotableProjects()))()
-    }, [])
-
     return (
         <>
             <BriefDescription/>
-            <NotableProjectsList projects={projects}/>
+            <NotableProjectsList/>
         </>
     )
 }
@@ -29,61 +17,51 @@ interface GithubProject {
     language: string
 }
 
-async function getNotableProjects() {
-    const notableProjects = [
-        {path: 'ennmichael/physics-pancake', language: 'ts'},
-        {path: 'ennmichael/marionette', language: 'python'},
-        {path: 'ennmichael/Bezier', language: 'c#'},
-        {path: 'ennmichael/gameboy', language: 'c++'},
-        {path: 'ennmichael/nes-emulator', language: 'c++'},
-        {path: 'ennmichael/pic16-tetris', language: 'c'}
-    ]
-
-    return Promise.all(notableProjects.map(async ({path, language}) => {
-        const {data: repo} = await axios.get(`https://api.github.com/repos/${path}`, {
-            headers: {'Accept': 'application/vnd.github.v3+json'}
-        })
-
-        return {
-            url: repo['html_url'],
-            name: repo['name'],
-            description: repo['description'],
-            language
-        }
-    }))
-}
-
 function BriefDescription() {
     return <></>
 }
 
-function NotableProjectsList({projects}: { projects: readonly GithubProject[] }) {
+function NotableProjectsList() {
+    const notableProjects = [
+        {name: 'physics-pancake', language: 'JS', description: 'A 2D physics simulation'},
+        {name: 'marionette', language: 'Python', description: 'A small Super Mario clone'},
+        {name: 'Bezier', language: 'C#', description: 'A simple program for drawing Bezier curves'},
+        {name: 'gameboy', language: 'C++', description: 'A snake and tetris clone'},
+        {name: 'nes-emulator', language: 'C++', description: 'An unfinished NES emulator'},
+        {name: 'pic16-tetris', language: 'C', description: 'A tetris program for a PIC microcontroller'}
+    ]
+
     return (
         <div>
-            {projects.map(project => (
-                <HoverPopover
-                    key={project.name} className="project"
-                    content={<p>{project.description}</p>} position="right"
-                >
-                    <a rel="noopener noreferrer" target="_blank" href={project.url}>
-                        <LanguageIcon language={project.language}/>
-                        {project.name}
-                    </a>
-                </HoverPopover>
-            ))}
+            {notableProjects.map(project => <Project {...project} />)}
         </div>
     )
 }
 
-function LanguageIcon({language}: { language: string }) {
-    const iconClasses: { [key: string]: string } = {
-        'python': 'devicon-python-plain',
-        'c#': 'devicon-csharp-plain',
-        'c++': 'devicon-cplusplus-plain',
-        'c': 'devicon-c-plain',
-        'ts': 'devicon-typescript-plain',
-        'elm': 'devicon-elm-plain'
+function Project(props: { name: string, language: string, description: string }) {
+    const [hovered, setHovered] = useState(false)
+
+    function onMouseEnter() {
+        setHovered(true)
     }
 
-    return <i className={`${iconClasses[language]} colored`}/>
+    function onMouseLeave() {
+        setHovered(false)
+    }
+
+    return (
+        <div className='project' key={props.name}>
+            <a className='project-title' rel="noopener noreferrer" target="_blank"
+               href={githubUrl(props.name)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                {props.name}
+            </a>
+            <div className={'project-details ' + (hovered ? 'hovered' : '')}>
+                <p>{props.language} | {props.description}</p>
+            </div>
+        </div>
+    )
+}
+
+function githubUrl(projectName: string): string {
+    return `https://github.com/ennmichael/${projectName}`
 }

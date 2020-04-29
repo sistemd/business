2fbcda4eace8e8cd06ebab250fd9b7cfcee1b399
Enn Michael <enntheprogrammer@gmail.com>
2020-04-29 19:10:46 +0200

    A stray .gitignore file

---
+++ build/precache-manifest.010d38179e10e293da7a14441e3b8c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee92efd198dbb3b37c3b8c140b67b0c",
+    "url": "/business/build/index.html"
+  },
+  {
+    "revision": "f8191c68e15a99a99b33",
+    "url": "/business/build/static/css/main.9a17c54e.chunk.css"
+  },
+  {
+    "revision": "95c45f6859d6d31d2784",
+    "url": "/business/build/static/js/2.b28b507e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/business/build/static/js/2.b28b507e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8191c68e15a99a99b33",
+    "url": "/business/build/static/js/main.08bbe36e.chunk.js"
+  },
+  {
+    "revision": "469e5ee6f2f842633fd2",
+    "url": "/business/build/static/js/runtime-main.c45606f0.js"
+  }
+]);

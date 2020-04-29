97a2ed475f1ec17e4764d897b95a8530fff11c4c
Enn Michael <enntheprogrammer@gmail.com>
2020-04-29 18:53:05 +0200

    Moved production build files from /build to / because Github is rude

---
+++ precache-manifest.d1d2f794867d3236ddf872b639122c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07153fa88089c3f1b92a1a97c6e516d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8191c68e15a99a99b33",
+    "url": "/static/css/main.9a17c54e.chunk.css"
+  },
+  {
+    "revision": "95c45f6859d6d31d2784",
+    "url": "/static/js/2.b28b507e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b28b507e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8191c68e15a99a99b33",
+    "url": "/static/js/main.08bbe36e.chunk.js"
+  },
+  {
+    "revision": "4a67aba8c62e56383ea7",
+    "url": "/static/js/runtime-main.6a60fa1c.js"
+  }
+]);
